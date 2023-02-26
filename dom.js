382a5dbf63cc7675528cf6c8544dc7723c2a8c31
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-02-26 08:53:19 +0530

    new element li added

--- dom.js
+++ dom.js
@@ -1,14 +1,16 @@
 
 const header = document.getElementById('main-header');
 header.style.borderBottom ='solid 3px black' ;
-const items = document.getElementsByClassName('list-group-item') ;
+// const items = document.getElementsByClassName('list-group-item') ;
+// for(const item of items){
+//     item.style.fontWeight = 'bold' ;
+//     item.style.backgroundColor ='gray' ;
+// }
+const items = document.getElementsByTagName('li') ;
 for(const item of items){
     item.style.fontWeight = 'bold' ;
-    item.style.color ='green' ;
+    item.style.backgroundColor ='gray' ;
 }
-items[2].style.backgroundColor ='green';
-for(const item of items){
-    item.style.fontWeight = 'bold' ;
-    item.style.color ='gray' ;
-}
+
+
 
