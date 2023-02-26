3ce07739ab3bc6a5fa6653ced2370868e1e59984
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-02-26 08:04:58 +0530

    new file index.html is added and also the main.js is updated

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const header = document.getElementById('main-header');
+header.style.borderBottom ='solid 3px black' ;
+const items = document.getElementsByClassName('list-group-item') ;
+for(const item of items){
+    item.style.fontWeight = 'bold' ;
+    item.style.color ='green' ;
+}
+
