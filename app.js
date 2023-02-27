6aacccf83a8fcd8c2e8cb722e0b489a3eeed967d
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-02-27 11:43:04 +0530

    app is now scalable

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const date = document.querySelector("#date");
 const time = document.querySelector("#time");
 const forminputs = document.querySelectorAll(".form-control");
 document.querySelector("#submitbtn").addEventListener("click", (e) => {
+    let userDetailesString = ""
+    const ul = document.getElementById('items') ;
+    const li = document.createElement('li');
   e.preventDefault();
   if (
     username.value != "" &&
@@ -15,10 +18,15 @@ document.querySelector("#submitbtn").addEventListener("click", (e) => {
   ) {
     const forminputs = document.querySelectorAll(".form-control");
     let userDetailes = {};
+    
     for (let item of forminputs) {
        userDetailes[item.name] = item.value ;
        item.value = '' ;
     }
     localStorage.setItem(userDetailes.email , JSON.stringify(userDetailes));
+    li.className = 'list-group-item' ;
+    li.innerText = `${userDetailes.name} ${userDetailes.email} ${userDetailes.phone} ${userDetailes.date} ${userDetailes.time}`
+    ul.appendChild(li);
+    
   }
 });
