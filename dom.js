1e7ebf1c0173ca1c91eb5b9d6094dae88fc4934a
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-02-27 09:10:19 +0530

    filter list item vai discription added

--- dom.js
+++ dom.js
@@ -2,6 +2,7 @@ const submitbtn = document.getElementById('submitbtn')
 submitbtn.addEventListener('click',(e)=>{
    e.preventDefault();
    const forminput = document.getElementById('forminput') ;
+   const discription = document.getElementById('discription') ;
    const ul = document.getElementById('items') ;
    const li = document.createElement('li');
    const deletebtn = document.createElement('button');
@@ -14,12 +15,13 @@ submitbtn.addEventListener('click',(e)=>{
    deletebtn.textContent = 'delete' ;
    deletebtn.className = 'btn btn-danger btn-sm float-right delete'
    li.className = 'list-group-item' ;
-   li.textContent = forminput.value ;
+   li.textContent = forminput.value + " " + discription.value;
    
    li.appendChild(deletebtn);
    li.appendChild(editbtn);
    ul.appendChild(li) ;
    forminput.value = '' ;
+   discription.value = '';
 
    
 } )
@@ -34,6 +36,20 @@ list.addEventListener('click' , (e)=>{
      
    }
    
+})
+
+const filter = document.getElementById('filter');
+filter.addEventListener('keyup' , (e)=>{
+   let filtertext = e.target.value.toLowerCase() ;
+   let listtext = list.children;
+   Array.from(listtext).forEach(item => {
+      var itemname = item.firstChild.textContent.toLowerCase();
+      if(itemname.indexOf(filtertext)!=-1){
+         item.style.display = 'block' ;
+      }else{
+         item.style.display = 'none' ;
+      }
+   });
 })
 
 
