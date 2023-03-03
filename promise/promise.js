330ca7d3fc2f19cdad67d73eb45113a64e1c9471
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-03-03 16:42:48 +0530

    promise.js file added

---
+++ promise/promise.js
@@ -0,0 +1,50 @@
+const posts = [{lastActivity: Date.now()}];
+function getPosts(){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            if(posts.length>0){
+                resolve(posts);
+            }else{
+                reject(`All the posts are deleted`);
+            }
+        },3000)
+    })
+}
+
+function updateLastUserActivityTime(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            posts[0].lastActivity = Date.now();
+            resolve(); 
+        },1000)
+    })
+}
+
+function createPost(post){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            posts.push(post);
+            resolve();
+        },2000)
+    })
+}
+
+function deletePost(){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            posts.pop();
+            resolve();
+        },1000)
+    })
+
+}
+Promise.all([updateLastUserActivityTime,createPost({title:"post 1"}),createPost({title:"post 2"})]).then(getPosts)
+.then((data)=> console.log(data)).then(deletePost).then(getPosts).then(data=>console.log(data));
+
+
+
+             
+
+
+
+
