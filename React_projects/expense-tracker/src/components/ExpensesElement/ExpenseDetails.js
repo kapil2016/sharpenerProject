55cfc6b40ead14140fa20ce8a1d6d8b09e1f8ea9
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-03-13 20:49:55 +0530

    delete button added

--- React_projects/expense-tracker/src/components/ExpensesElement/ExpenseDetails.js
+++ React_projects/expense-tracker/src/components/ExpensesElement/ExpenseDetails.js
@@ -1,10 +1,17 @@
 import "./ExpenseItem.css";
- const ExpenseDetails = (props)=> {
+ const ExpenseDetails = (props)=>{
+   function clickHendler(e){
+    const item = e.target.parentNode.parentNode.parentNode  ;
+    item.removeChild(e.target.parentNode.parentNode );
+    console.log(item);
+   }
+
   return (
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <h2>{props.LocationOfExpenditure}</h2>
       <div className="expense-item__price">{props.price}</div>
+      <button onClick={clickHendler}>Delete Expense</button>
     </div>
   );
 }
