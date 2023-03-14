4339c145e1154495edb443095d1bf7536fc1a749
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-03-14 11:49:48 +0530

    new button is added to change the amount to 100

--- React_projects/expense-tracker/src/components/ExpensesElement/ExpenseDetails.js
+++ React_projects/expense-tracker/src/components/ExpensesElement/ExpenseDetails.js
@@ -1,16 +1,26 @@
 import "./ExpenseItem.css";
+import { useState } from "react";
  const ExpenseDetails = (props)=>{
+    const [currAmount, setAmount]  = useState(props.price) ;
    function clickHendler(e){
     const item = e.target.parentNode.parentNode.parentNode  ;
     item.removeChild(e.target.parentNode.parentNode );
     console.log(item);
    }
 
+   function changeExpense () {
+      setAmount((currAmount)=>{
+        currAmount = `$100` ;
+        return currAmount ;
+      })
+   }
+
   return (
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <h2>{props.LocationOfExpenditure}</h2>
-      <div className="expense-item__price">{props.price}</div>
+      <div className="expense-item__price">{currAmount}</div>
+      <button onClick={changeExpense}>Change Expense</button>
       <button onClick={clickHendler}>Delete Expense</button>
     </div>
   );
